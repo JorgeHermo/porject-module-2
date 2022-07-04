f1b0e208da40259c869414c7e0f8d4ba4cb9ffc9
Jorge <jhermotorrado@gmail.com>
2022-07-04 18:43:07 +0200

    minor changes index.routes and edit.hbs

--- ReciApi/routes/index.routes.js
+++ ReciApi/routes/index.routes.js
@@ -2,9 +2,7 @@ module.exports = app => {
   app.use('/', require('./base.routes'))
   app.use('/auth', require('./auth.routes'))
   app.use('/users', require('./user.routes'))
-  /* 
   app.use('/recipes', require('./recipe.routes'))
-  /* app.use('/users', require('./user.routes'))
-  app.use('/admin', require('./admin.routes'))
-  app.use('/maps', require('./maps.routes')) */
+  /*   app.use('/admin', require('./admin.routes'))
+    app.use('/maps', require('./maps.routes')) */
 }
