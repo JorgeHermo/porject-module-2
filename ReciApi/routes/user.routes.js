23d7e68be48c0b653585912a97ef018f27ee5f65
Marcos del Olivo <delolivolopez@gmail.com>
2022-07-04 20:19:10 +0200

    update CRUD-Users

--- ReciApi/routes/user.routes.js
+++ ReciApi/routes/user.routes.js
@@ -11,12 +11,14 @@ const User = require("../models/User.model")
 // | -----------------   | ---------   | -------------------------------   |
 // | `/users` | GET | Retrive the user list data | DONE
 // | `/user/:id/details` | GET | Retrive the user details | DONE
-// | `/user/:id/edit` | GET | Retrives the user Data |
-// | `/user/:id/edit` | POST | Update a specific User |
-// | `/user/:id/delete` | POST | Delete a specific User |
+// | `/user/:id/edit` | GET | Retrives the user Data | DONE
+// | `/user/:id/edit` | POST | Update a specific User | DONE
+// | `/user/:id/delete` | POST | Delete a specific User | DONE
 
 
 ///////////////////////
+
+//ALL USERS
 
 router.get('/', (req, res, next) => {
 
@@ -26,6 +28,7 @@ router.get('/', (req, res, next) => {
         .catch(err => console.log(err))
 })
 
+//USER DETAILS
 router.get('/:id/details', (req, res, next) => {
 
     const { id } = req.params   
@@ -36,6 +39,7 @@ router.get('/:id/details', (req, res, next) => {
         .catch(err => console.log(err))
 })
 
+//EDIT USER (RENDER)
 router.get('/:id/edit', (req, res, next) => {
 
     const { id } = req.params
@@ -45,21 +49,34 @@ router.get('/:id/edit', (req, res, next) => {
         .then(user => res.render('users/user-update', { user }))
         .catch(err => console.log(err))
 })
-/////////////////////////////////
 
-// router.post('/:id/edit', (req, res, next) => {
+//EDIT USER (HANDLE)
 
-//     const { username, email, password, avatar, description, role } = req.body
-//     const { id } = req.query
+router.post('/:id/edit', (req, res, next) => {
 
-//     User
-//         .findByIdAndUpdate(id, { username, email, password, avatar, description, role })
-//         .then(user => res.redirect('/users/${user._id}'))
-//         .catch(err => console.log(err))
+
+    const { username, email, password, avatar, description, role } = req.body
+    const { id } = req.params
+
+    User
+        .findByIdAndUpdate(id, { username, email, password, avatar, description, role })
+        .then(() => res.redirect(`/users/${id}/details`))
+        .catch(err => console.log(err))
 
        
-// })
+})
 
+//DELETE USER 
+
+router.get('/:id/delete', (req, res) => {
+
+    const { id } = req.params
+
+    User
+        .findByIdAndDelete(id)
+        .then(() => res.redirect(`/`))
+        .catch(err => console.log(err))
+})
 
 
 
