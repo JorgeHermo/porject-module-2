ef2370a320c868b771fbd39e878b1f430b2f5645
Jorge <jhermotorrado@gmail.com>
2022-07-05 19:40:28 +0200

    Jorge made a big mistake

--- ReciApi/routes/index.routes.js
+++ ReciApi/routes/index.routes.js
@@ -3,6 +3,7 @@ module.exports = app => {
   app.use('/', require('./auth.routes'))
   app.use('/users', require('./user.routes'))
   app.use('/recipes', require('./recipe.routes'))
+  app.use('/api', require('./api.routes'))
   /*   app.use('/admin', require('./admin.routes'))
   app.use('/maps', require('./maps.routes')) */
 }
